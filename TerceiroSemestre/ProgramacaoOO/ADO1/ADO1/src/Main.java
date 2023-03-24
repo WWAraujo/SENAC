634bf38c41001582680324dd918286441e549e49
WWAraujo <103542753+WWAraujo@users.noreply.github.com>
2023-03-24 16:18:41 -0300

    Projeto

--- TerceiroSemestre/ProgramacaoOO/ADO1/ADO1/src/Main.java
+++ TerceiroSemestre/ProgramacaoOO/ADO1/ADO1/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
     }
+
+    //1- Criar um método que retorne a referencia bibliográfica de um autor
+
+    //entra com um nome exemplo: Ricardo Luiz da Silva Passos e sai Luiz da Silva Passos, Ricardo
+
+    //2 - Criar um método que conte quantas vezes uma palavra aparece numa frase
+
+    //Exemplo entra uma frase: Ana come banana, quantidade de vezes que apareceu a palrava ana foram 3
 }
