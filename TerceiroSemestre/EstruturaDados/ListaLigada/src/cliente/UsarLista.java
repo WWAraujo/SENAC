bc918f2b57cca03899a2169113e07c4971d349f6
Wallace Wagner <103542753+WWAraujo@users.noreply.github.com>
2023-03-28 22:20:41 -0300

    Busca e remocao da lista Ligada Simples

--- TerceiroSemestre/EstruturaDados/ListaLigada/src/cliente/UsarLista.java
+++ TerceiroSemestre/EstruturaDados/ListaLigada/src/cliente/UsarLista.java
@@ -8,10 +8,24 @@ public class UsarLista {
     public static void main(String[] args) {
         Lista lista = new Lista();
         lista.inserir(new Contato("Wallace","1234-5678"));
-        lista.inserir(new Contato("Wagner","1234-5678"));
-        lista.inserir(new Contato("Araujo","1234-5678"));
+        lista.inserir(new Contato("Wagner ","1234-5678"));
+        lista.inserir(new Contato("Araujo ","1234-5678"));
         lista.inserir(new Contato("Pereira","1234-5678"));
+
+//        Object obj = lista.buscar(1);
+//        if(obj!=null){
+//            System.out.println(obj);
+//        }else {
+//            System.out.println("Nao encontrado!");
+//        }
+//        lista.encontrarMostrar(1);
+
+        lista.mostrar();
+        System.out.println("==========================================");
+
+        lista.remover(3);
 
         lista.mostrar();
     }
+
 }
--- TerceiroSemestre/EstruturaDados/ListaLigada/src/lista/Lista.java
+++ TerceiroSemestre/EstruturaDados/ListaLigada/src/lista/Lista.java
@@ -3,7 +3,10 @@ package lista;
 public class Lista {
 
     private Elemento inicio, atual, aux;
+    private int v = 1;
     public void inserir(Object objeto){
+        Contato c = (Contato) objeto;
+        c.setId(v++);
 
         if(inicio == null){
             inicio = new Elemento(objeto,null);
@@ -23,4 +26,53 @@ public class Lista {
         }
     }
 
+    public void encontrarMostrar(int id){
+        Elemento e = inicio;
+        Contato c;
+        while (e!=null){
+            c=(Contato)e.getObjeto();
+            if (id== c.getId()){
+                System.out.println(e.getObjeto());
+                break;
+            }
+            e = e.getProx();
+        }
+    }
+
+    public Object[] buscar(int id){
+        Object[] v = new Object[3];
+
+        Elemento e = inicio;
+        Contato c;
+        while (e!=null){
+            c=(Contato)e.getObjeto();
+            if (id== c.getId()){
+                v[0] = e;
+                v[2] = c;
+                return v;
+            }
+            e = e.getProx();
+        }
+        return null;
+    }
+
+    public void remover(int id){
+        Object[] v = buscar(id);
+        Object[] v2 = buscar(id-1);
+        if(v != null){
+            Elemento e = (Elemento) v[0];
+            if(e == inicio){//caso 1 - remocao do primeiro elemento
+                inicio = e.getProx();
+                e.setProx(null);
+            } else if (e.getProx()==null){
+                Elemento e2 = (Elemento) v2[0];
+                e2.setProx(e.getProx());
+            } else {
+                Elemento e2 = (Elemento) v2[0];
+                e2.setProx(e.getProx());
+            }
+        } else {
+            System.out.println("Erro ao remover! Nao encontrato");
+        }
+    }
 }
