95c814279d4fdcdddf59e55c023482c8a8be639f
wallace.waraujo <wallace20130@gmail.com>
2023-05-12 20:28:42 -0300

    aula11

---
+++ TerceiroSemestre/ProgramacaoOO/Aula11/src/main/java/org/example/PessoaJava.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface PessoaJava {
+
+    public void adicionar();
+    public void pesquisar();
+    public void excluir();
+    public double DESCONTO = 0.10;
+
+}
